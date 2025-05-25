58ed18b6a017f9f0045463270e413585e451eca5
Jordy Stvaer <jerco27@gmail.com>
2025-05-25 18:49:27 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <InitialInfoForm>
    
    with the text content:
    
        Solicitud de ChequeNE (Seguimiento NX1) *Referencia (Contenedor, Guía, BL, Factura...)De (Nombre colaborador) *Fecha *Seleccione una fechaA: *ContabilidadContinuar
    
    Relevant files:
    
        - https://6000-firebase-studio-1748189786732.cluster-ux5mmlia3zhhask7riihruxydo.cloudworkstations.dev/_next/static/chunks/node_modules_086cb224._.js
        - https://6000-firebase-studio-1748189786732.cluster-ux5mmlia3zhhask7riihruxydo.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1748189786732.cluster-ux5mmlia3zhhask7riihruxydo.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    organizemos, debajo del titulo primero "A" le sigue "De (Nombre colaborador):" NE (Seguimiento NX1) y Referencia

--- src/components/examiner/InitialInfoForm.tsx
+++ src/components/examiner/InitialInfoForm.tsx
@@ -71,6 +71,83 @@ function onSubmit(data: InitialInfoFormData) {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <FormField
                 control={form.control}
+                name="recipient"
+                render={({ field }) => (
+                  <FormItem className="md:col-span-2">
+                    <FormLabel>A: *</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Destinatario o Departamento" {...field} value={field.value ?? ''} />
+                    </FormControl>
+                      <Button
+                        type="button"
+                        variant="outline"
+                        size="sm"
+                        onClick={() => form.setValue('recipient', 'Contabilidad', { shouldValidate: true })}
+                        className="mt-1 text-xs"
+                      >
+                        Contabilidad
+                      </Button>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="manager"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>De (Nombre colaborador) *</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Nombre completo del colaborador" {...field} value={field.value ?? ''} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="date"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel>Fecha *</FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant={"outline"}
+                            className={cn(
+                              "w-full pl-3 text-left font-normal",
+                              !field.value && "text-muted-foreground"
+                            )}
+                          >
+                            {field.value ? (
+                              format(field.value, "PPP", { locale: es })
+                            ) : (
+                              <span>Seleccione una fecha</span>
+                            )}
+                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          mode="single"
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          disabled={(date) =>
+                            date > new Date() || date < new Date("1900-01-01")
+                          }
+                          initialFocus
+                          locale={es}
+                        />
+                      </PopoverContent>
+                    </Popover>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
                 name="ne"
                 render={({ field }) => (
                   <FormItem>
@@ -95,83 +172,6 @@ function onSubmit(data: InitialInfoFormData) {
                   </FormItem>
                 )}
               />
-              <FormField
-                control={form.control}
-                name="manager"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>De (Nombre colaborador) *</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Nombre completo del colaborador" {...field} value={field.value ?? ''} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-                <FormField
-                  control={form.control}
-                  name="date"
-                  render={({ field }) => (
-                    <FormItem className="flex flex-col">
-                      <FormLabel>Fecha *</FormLabel>
-                      <Popover>
-                        <PopoverTrigger asChild>
-                          <FormControl>
-                            <Button
-                              variant={"outline"}
-                              className={cn(
-                                "w-full pl-3 text-left font-normal",
-                                !field.value && "text-muted-foreground"
-                              )}
-                            >
-                              {field.value ? (
-                                format(field.value, "PPP", { locale: es })
-                              ) : (
-                                <span>Seleccione una fecha</span>
-                              )}
-                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                            </Button>
-                          </FormControl>
-                        </PopoverTrigger>
-                        <PopoverContent className="w-auto p-0" align="start">
-                          <Calendar
-                            mode="single"
-                            selected={field.value}
-                            onSelect={field.onChange}
-                            disabled={(date) =>
-                              date > new Date() || date < new Date("1900-01-01")
-                            }
-                            initialFocus
-                            locale={es}
-                          />
-                        </PopoverContent>
-                      </Popover>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="recipient"
-                  render={({ field }) => (
-                    <FormItem className="md:col-span-2">
-                      <FormLabel>A: *</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Destinatario o Departamento" {...field} value={field.value ?? ''} />
-                      </FormControl>
-                       <Button
-                          type="button"
-                          variant="outline"
-                          size="sm"
-                          onClick={() => form.setValue('recipient', 'Contabilidad', { shouldValidate: true })}
-                          className="mt-1 text-xs"
-                        >
-                          Contabilidad
-                        </Button>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
             </div>
             <div className="flex justify-end pt-2">
               <Button type="submit" className="btn-primary px-6 py-3">
